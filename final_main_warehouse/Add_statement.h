e1efe04287d1094dd5ffaac9e4a869c6368d2ce7
PUN <punnawat.mungkalarungsi@gmail.com>
2023-12-04 12:11:26 +0700

    Fix basket file handling and update statement generation

--- final_main_warehouse/Add_statement.h
+++ final_main_warehouse/Add_statement.h
@@ -8,8 +8,10 @@ void  add_statement( int total_cost, int totl_profit)
     FILE *state_file = fopen("statement.csv", "a");
     FILE *basketFile = fopen("basket.txt", "r");
     if (basketFile == NULL) {
+        
         printf("(* Your basket is empty *)");
         fclose(state_file);
+        fclose(basketFile);
         return;
     }
     time_t timer;
@@ -22,7 +24,7 @@ void  add_statement( int total_cost, int totl_profit)
     strftime(time_n, 26, "%Y-%m-%d,%H:%M:%S", tm_info);
     printf("%s\n", time_n);
 
-    int unit_basket;
+    int unit_basket, index = 0;
     char name_basket[100], code_basket[100];
     char line[50];
     double cost, sell;
@@ -45,11 +47,15 @@ void  add_statement( int total_cost, int totl_profit)
             token = strtok(NULL, "-");
             col++;
         }
+        index++;
         fprintf(state_file,"%s,%s,%d,%s,%lf,%lf\n", time_n, code_basket, unit_basket, name_basket, cost, sell);
         //printf("%s,%d,%s,%lf,%lf,%s\n", code_basket, unit_basket, name_basket, cost, sell, time_n);
     }
-    fprintf(state_file, "%s,%s,%d,%s,%s,%d\n", time_n, "Total", total_cost, "", "Profit", totl_profit);
-    fprintf(state_file, ",,,,,,\n");
+    if (index != 0 && fgets(line, sizeof(line), basketFile) == NULL)
+    {
+        fprintf(state_file, "%s,%s,%d,%s,%s,%d\n", time_n, "Total", total_cost, "", "Profit", totl_profit);
+        fprintf(state_file, ",,,,,,\n");
+    }
 
     fclose(basketFile);
     fclose(state_file);
